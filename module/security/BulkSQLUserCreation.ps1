24b48a839e166f6b7adbcb252f6440abcf4a7d7f
erica-poirier <epoirier@devolutions.net>
2021-05-06 13:41:35 -0400

    Bulk SQL AD user creation

---
+++ module/security/BulkSQLUserCreation.ps1
@@ -0,0 +1,84 @@
+﻿#requires -version 4
+<#
+.SYNOPSIS
+  Creates RDM user accounts from AD accounts stored in a CSV file.
+.DESCRIPTION
+  Creates RDM user accounts from AD accounts stored in a CSV file.
+.PARAMETER <Parameter_Name>
+  None
+.INPUTS
+  CSV file that contains the list of AD user accounts.
+.OUTPUTS
+  None
+.NOTES
+  Version:        1.0
+  Author:         Érice Poirier
+  Creation Date:  2021-04-29
+  Purpose/Change: Initial script development
+.EXAMPLE
+  
+#>
+
+
+#-------------------------------[Initialisations]------------------------------
+
+#Set Error Action to Silently Continue
+$ErrorActionPreference = 'SilentlyContinue'
+
+#Import Modules & Snap-ins
+Import-Module RemoteDesktopManager.PowerShellModule
+
+
+#---------------------------------[Functions]----------------------------------
+
+#---------------------------------[Execution]----------------------------------
+
+### Connect to the SQL Data Source
+$ds = Get-RDMDataSource -Name "YourSQLDataSourceNameHere"
+Set-RDMCurrentDataSource $ds
+Update-RDMUI
+
+$path     = Split-Path -parent $MyInvocation.MyCommand.Definition  
+$urspath  = $path + "\user_input.csv" 
+$csv      = @() 
+$csv      = Import-Csv -Path $urspath 
+
+foreach ($user in $csv)            
+{            
+    $Displayname = $User.Firstname + " " + $User.Lastname            
+    $UserFirstname = $User.Firstname            
+    $UserLastname = $User.Lastname            
+    $Email = $User.Firstname + "." + $User.Lastname + "@" + $User.Maildomain
+    
+    # Replace the domain name below.
+    $NetBios = "YourDomain\" + $SAM
+    try
+    {
+        # NetBios username format
+        # $newUser = New-RDMUser -Login $NetBios -Email $Email -AuthentificationType SqlServer -IntegratedSecurity
+        
+        # UPN username format
+        $newUser = New-RDMUser -Login $Email -Email $Email -AuthentificationType SqlServer -IntegratedSecurity 
+        $newUser.UserType = "User"
+        $newUser.FirstName = $UserFirstname
+        $newUser.LastName = $UserLastname
+        Set-RDMUser -User $newUser
+
+        Write-Host "$Displayname created"
+    }
+    catch
+    {
+        Write-Host "Unable to create user $Displayname"
+    }
+} 
+
+Write-Host "Done!!!"
+
+#----------------------------------[Closure]-----------------------------------
+# If running in the console only, ask to press a key
+if ($Host.Name -eq "ConsoleHost")
+{
+    Write-Host "Press any key to continue..."
+    $Host.UI.RawUI.FlushInputBuffer()  
+    $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp") > $null
+}
