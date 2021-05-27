d0c66229aa6e78b3b394c4777e36009f6afd1b15
Eric St-Martin <estmartin@devolutions.net>
2021-05-27 15:36:47 -0400

    Mass Edit Script Samples
    -Changes to Sub-connections Sample
    Information can be tweaked to change the Entry type, what you're changing. 
    
    -Edit Entry's Password in All Vaults
    Made to edit a shared credential entry in all vaults.

---
+++ module/entries/MassEdits/ChangesTo-Subconnections-Sample.ps1
@@ -0,0 +1,76 @@
+#requires -version 4
+<#
+.SYNOPSIS
+  Mass edit SubConnections (parent/childs) in RDM
+
+.DESCRIPTION
+  Mass edit SubConnections in RDM by Un-Parenting SubConnections by ConnectionType, 
+  then make the needed edits to the entry and then re-parenting the entry
+
+.PARAMETER <Parameter_Name>
+
+.INPUTS
+  Confirmation will be required for EACH Un-Parent
+
+.OUTPUTS
+  None
+
+.NOTES
+  Version:        1.0
+  Author:         Eric St-Martin
+  Creation Date:  05/05/2021
+  Purpose/Change: Initial script development
+.EXAMPLE
+
+#>
+
+#-------------------------------[Initialisations]------------------------------
+
+#Set Error Action to Silently Continue
+$ErrorActionPreference = 'SilentlyContinue'
+
+#Import Modules & Snap-ins
+Import-Module RemoteDesktopManager.PowerShellModule
+
+#---------------------------------[Functions]----------------------------------
+
+
+#---------------------------------[Execution]----------------------------------
+
+# Getting a list of Entries that have SubConnections
+$parents = Get-RDMSession | Where-Object SubConnections
+
+# Run the changes
+foreach ($parent in $parents)
+{
+      # Gets the SubConnections where ConnectionType in this case is Sftp
+      # This can be tweaked to fit any need. 
+
+      $subcons = $parent.SubConnections | Where-Object ConnectionType -eq "Sftp"
+      $child = $subcons
+
+      # Runs the UnParent
+      # **PS: There will be a Confirmation prompt for each entry being un-parented
+      Invoke-RDMUnparentSession -Session $subcons
+
+      # Changes the Connection type to FTP but again can be tweaked to change anything
+      $child.ConnectionType = "Ftp"
+
+      # Saves changes made above
+      Set-RDMSession $child -Refresh
+
+      # In this case we wanted to re-parent the entry
+      Invoke-RDMParentSession -Session $child -ParentSession $parent  
+}
+Update-RDMUI
+
+Write-Host "Changes completed!"
+
+#----------------------------------[Closure]-----------------------------------
+# If running in the console only, ask to press a key
+if ($Host.Name -eq "ConsoleHost")
+{
+    Write-Host "Press any key to continue..."
+    $Host.UI.RawUI.FlushInputBuffer()  
+    $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp") > $null
+}
---
+++ module/entries/MassEdits/Edit-EntryPassword-All-Vaults.ps1
@@ -0,0 +1,64 @@
+#requires -version 4
+<#
+.SYNOPSIS
+  Change password on an individual Credential entry shared between all Vaults
+
+.DESCRIPTION
+  Option to change all Credential entries password sharing the same name across all Vaults
+  
+  SN: This is not a secure option we recommend having a seperate Vault with shared credentials 
+
+.PARAMETER <Parameter_Name>
+  None
+
+.INPUTS
+  Confirmation will be required for EACH Un-Parent
+
+.OUTPUTS
+  None
+
+.NOTES
+  Version:        1.0
+  Author:         Eric St-Martin
+  Creation Date:  05/05/2021
+  Purpose/Change: Initial script development
+.EXAMPLE
+
+#>
+
+#-------------------------------[Initialisations]------------------------------
+
+#Set Error Action to Silently Continue
+$ErrorActionPreference = 'SilentlyContinue'
+
+#Import Modules & Snap-ins
+Import-Module RemoteDesktopManager.PowerShellModule
+
+#---------------------------------[Functions]----------------------------------
+
+
+#---------------------------------[Execution]----------------------------------
+
+$repos = Get-RDMRepository
+$newPass = Read-Host "Enter new password" -AsSecureString
+
+foreach ($r in $repos)
+{
+    Set-RDMCurrentRepository $r
+    $creds = Get-RDMSession | Where-Object {$_.Name -EQ "ChangeME" -and $_.ConnectionType -eq "Credential"}
+    Set-RDMSessionPassword $creds.ID -Password $newPass
+
+    Write-Host $r.Name"\"$creds.Name"password has been successfully change."
+}
+Update-RDMUI
+
+Write-Host "Changes completed!"
+
+#----------------------------------[Closure]-----------------------------------
+# If running in the console only, ask to press a key
+if ($Host.Name -eq "ConsoleHost")
+{
+    Write-Host "Press any key to continue..."
+    $Host.UI.RawUI.FlushInputBuffer()  
+    $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp") > $null
+}
