97124e024ce5d0e5fead4d3d74243a123e891a0d
Richard Boisvert <rboisvert@devolutions.net>
2023-04-13 14:27:30 -0400

    change PowerShell module name
    if(-not (Get-Module Devolutions.PowerShell -ListAvailable)){
        Install-Module Devolutions.PowerShell -Scope CurrentUser
    }

--- module/User/ModifyUserTypeRDM14_3AndLower.ps1
+++ module/User/ModifyUserTypeRDM14_3AndLower.ps1
@@ -1,7 +1,7 @@
 #The following script will modify the User type property from Read only to User.
 #check if RDM PS module is installed
-if(-not (Get-Module RemoteDesktopManager -ListAvailable)){
-	Install-Module RemoteDesktopManager -Scope CurrentUser
+if(-not (Get-Module Devolutions.PowerShell -ListAvailable)){
+    Install-Module Devolutions.PowerShell -Scope CurrentUser
 }
 
 # Adapt the data source name
